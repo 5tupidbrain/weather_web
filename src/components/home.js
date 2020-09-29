6c14a8e9754eb6acd5178a2b342c470d556faddc
Saransh Hardaha <hellosaranshh@gmail.com>
2020-09-29 12:40:57 +0530

    added -c/f -w_info -searchfix

--- src/components/home.js
+++ src/components/home.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import search_l from '../assets/search-location-solid.svg';
-import thermo from '../assets/thermometer-half-solid.svg';
 
 class Home extends Component {
+
     constructor() {
         super();
         this.state = {
@@ -11,41 +11,84 @@ class Home extends Component {
     }
 
     dt_tm() {
-        var op_dt = { year: 'numeric', month: 'long', day: 'numeric' };
-        var dt = new Date();
-        var ampm = dt.getHours() >= 12 ? 'PM' : 'AM';
-        var tm = dt.getHours() % 12 + " : " + dt.getMinutes() + " " + ampm;
+        let op_dt = { year: 'numeric', month: 'long', day: 'numeric' };
+        let dt = new Date();
+        let ampm = dt.getHours() >= 12 ? 'PM' : 'AM';
+        let tm = (dt.getHours() % 12 === 0 ? '12' : dt.getHours() % 12) + " : " + dt.getMinutes() + " " + ampm;
         document.getElementById("dt").innerHTML = dt.toLocaleString("en-US", op_dt);
         document.getElementById("tm").innerHTML = tm.toLocaleString("en-US");
     }
+    // ip_api() {
+    //     let ip_url = 'https://ipapi.co/json/';
+    //     let da;
+    //     fetch(ip_url)
+    //         .then(response => response.json())
+    //         .then(data => da = data)
+    //         .then(() => {
+    //             document.getElementById('location').value = da.city;
+    //         }
+    //         );
+    // }
 
-    weather_api() {
+    async weather_api() {
+        let location = document.getElementById('location').value || 'Jabalpur';
+        let api_key = '3faadbb73e7e4c4fb09132852202809';
+        let days = 1;
+        let api = `//api.weatherapi.com/v1/current.json?key=${api_key}&q=${location}&days=${days}`;
 
-        var location = document.getElementById('location').value || 'Jabalpur';
-        var api_key = '3faadbb73e7e4c4fb09132852202809';
-        var days=1;
-        var api = `//api.weatherapi.com/v1/current.json?key=${api_key}&q=${location}&days=${days}`;
-        console.log(this.state.city);
+        let obj;
+        await fetch(api).then(res => res.json()).then(data => obj = data)
+            .then(() => {
+                let loc = obj.location.name;
+                let loc_r = obj.location.region;
+                let temp_c = obj.current.feelslike_c;
+                let condition = obj.current.condition.text;
+                let wind_dir = obj.current.wind_dir;
+                let wind_kph = obj.current.wind_kph;
+                let wind_mph = obj.current.wind_mph;
+                let humidity = obj.current.humidity;
+                let cloud = obj.current.cloud;
 
-        var obj;
-        fetch(api).then(res => res.json()).then(data => obj = data)
-        .then(() => {
-            var loc= obj.location.name;
-            var loc_r=obj.location.region;
-            var temp_c=obj.current.temp_c;
-            var condition=obj.current.condition.text;
+                document.getElementById('loc').innerHTML = `${loc}, ${loc_r}`;
+                document.getElementById('deg').innerHTML = `${temp_c}<sup>&deg;C</sup>`;
+                document.getElementById('condition').innerHTML = condition;
+                document.getElementById('wind_dir').innerHTML = wind_dir;
+                document.getElementById('wind_kph').innerHTML = wind_kph;
+                document.getElementById('wind_mph').innerHTML = wind_mph;
+                document.getElementById('humidity').innerHTML = `${humidity}%`;
+                document.getElementById('cloud').innerHTML = `${cloud}%`;
 
-            document.getElementById('loc').innerHTML=`${loc}, ${loc_r}`;
-            document.getElementById('deg').innerHTML=`${temp_c}<span>&deg;</span>`;
-            document.getElementById('condition').innerHTML=condition;
-        });
-       
+            });
+    }
+
+    change_temp() {
+        let deg = document.getElementById('deg');
+        if (deg.innerHTML.includes('C')) {
+            let kelvin = ((parseFloat(deg.innerHTML) * 9 / 5) + 32).toFixed(1);
+            let temp = (kelvin).includes(.0) ? parseInt(kelvin) : kelvin;
+            deg.innerHTML = `${temp}<sup>&deg;F</sup>`;
+        }
+        else {
+            let celsius = ((parseFloat(deg.innerHTML) - 32) * 5 / 9).toFixed(1);
+            let temp = (celsius).includes(.0) ? parseInt(celsius) : celsius;
+            deg.innerHTML = `${temp}<sup>&deg;C</sup>`;
+        }
     }
 
 
     componentDidMount() {
         this.dt_tm();
+        // this.ip_api();
         this.weather_api();
+
+        let input = document.getElementById("location");
+        input.addEventListener("keyup", function (event) {
+            // Number 13 is the "Enter" key on the keyboard
+            if (event.keyCode === 13) {
+                event.preventDefault();
+                document.getElementById("search_loc").click();
+            }
+        });
     }
 
     render() {
@@ -53,13 +96,11 @@ class Home extends Component {
             <div className='home'>
 
                 <div className='nav'>
-                    <button id='thermo'>
-                        <img src={thermo} height='32px' alt="Logo" />
-                        <br />
+                    <button id='thermo' onClick={() => this.change_temp()}>
                         &deg;C/F
                     </button>
-                    <input type='text' id='location' placeholder='Search location' onSubmit={() => this.weather_api()}></input>
-                    <button id='search_loc' type='submit' onClick={() => this.weather_api()}>
+                    <input type='text' id='location' placeholder='Search location'></input>
+                    <button id='search_loc' type='supmit' onClick={() => this.weather_api()}>
                         <img src={search_l} alt="Logo" />
                     </button>
                 </div>
@@ -68,7 +109,7 @@ class Home extends Component {
                 <br />
 
                 <div className='i_deg'>
-                    <h1 className='deg' id='deg'>--<span>&deg;</span></h1>
+                    <h1 className='deg' id='deg'>--<sup>&deg; C</sup></h1>
                     <p id='condition'>------</p>
                     <br />
                 </div>
@@ -80,11 +121,36 @@ class Home extends Component {
                 <br />
 
                 <div className='w_info'>
-                    <div className='card w_week'>
+                    <div className='card winfo'>
 
+                        <div className='wind_i flex_m'>
+                            <div className='wind_dir'>
+                                <h2 id='wind_dir'>- -</h2>
+                                <p>Wind Direction</p>
+                            </div>
+                            <div>
+                                <h4 id='wind_kph'>- -</h4>
+                                <p>Wind(Kph)</p>
+                                <br />
+                                <h4 id='wind_mph'>- -</h4>
+                                <p>Wind(Mph)</p>
+                            </div>
+                        </div>
+
+                        <div className='flex_m other_i'>
+                            <div>
+                                <h3 id='cloud'>- -</h3>
+                                <p>Humidity</p>
+                            </div>
+                            <div>
+                                <h3 id='humidity'>- -</h3>
+                                <p>cloud</p>
+                            </div>
+
+                        </div>
                     </div>
                     <br />
-                    <div className='card w_forecast'>
+                    <div className='card w_week'>
 
                     </div>
                 </div>
